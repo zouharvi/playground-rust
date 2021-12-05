4f3a88b9b03d81d5a382207ccf24d3885e64fe76
Vilém Zouhar <vilem.zouhar@gmail.com>
2021-12-05 11:01:55 +0100

    day 5

---
+++ 2021/day_05/main2.rs
@@ -0,0 +1,93 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+struct Line {
+    x0: usize,
+    x1: usize,
+    y0: usize,
+    y1: usize,
+}
+
+fn main() {
+    let file = File::open("day_05/input2").unwrap();
+    let reader = BufReader::new(file);
+    let mut lines = reader
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let line_vec = line.split(" -> ").collect::<Vec<&str>>();
+            let seg0 = line_vec[0].split(',').collect::<Vec<&str>>();
+            let seg1 = line_vec[1].split(',').collect::<Vec<&str>>();
+            Line {
+                x0 : seg0[0].parse().unwrap(),
+                y0 : seg0[1].parse().unwrap(),
+                x1 : seg1[0].parse().unwrap(),
+                y1 : seg1[1].parse().unwrap(),
+            }
+        })
+        .collect::<Vec<Line>>();
+
+    // just use the constant 1000 for now
+    let mut data = vec![vec![0; 1000]; 1000];
+
+    for line in lines.iter_mut() {
+        // handle non-straight lines
+        if (line.x0 != line.x1) && (line.y0 != line.y1) {
+            
+            // normalize lines so that everything begins on the left
+            if line.x0 > line.x1 {
+                let tmp = line.x0;
+                line.x0 = line.x1;
+                line.x1 = tmp;
+                let tmp = line.y0;
+                line.y0 = line.y1;
+                line.y1 = tmp;
+            }
+            let go_down = line.y1 > line.y0;
+
+            // todo: really +1?
+            for i in 0..(line.x1-line.x0+1) {
+                if go_down {
+                    data[line.y0+i][line.x0+i] += 1;
+                } else {
+                    data[line.y0-i][line.x0+i] += 1;
+                }
+            }
+        } else {
+            // normalize lines
+            if line.x0 > line.x1 {
+                let tmp = line.x0;
+                line.x0 = line.x1;
+                line.x1 = tmp;
+            }
+            if line.y0 > line.y1 {
+                let tmp = line.y0;
+                line.y0 = line.y1;
+                line.y1 = tmp;
+            }
+
+            // this fills a rectangle but if we are given only straight lines it works
+            for col_i in line.x0..(line.x1+1) {
+                for row_i in line.y0..(line.y1+1) {
+                    data[row_i][col_i] += 1;
+                }
+            }
+        }
+
+    }
+
+
+
+    let mut total_dangerous = 0;
+    for row in data {
+        for val in row {
+            // print!("{}", val);
+            if val >= 2 {
+                total_dangerous += 1;
+            }
+        }
+        // println!();
+    }
+
+    println!("{}", total_dangerous);
+}
